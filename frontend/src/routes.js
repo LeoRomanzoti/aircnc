e21ccd4e13c445a8c111c491e678ece80669324e
LeoRomanzoti <leo.romanzoti@gmail.com>
2022-04-28 22:28:35 -0700

    meu primeiro commit

---
+++ frontend/src/routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import New from './pages/New';
+
+
+export default function Rotas() {
+    return (
+        <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/new" element={<New />} />
+          </Routes>
+        </BrowserRouter>
+    );
+}
